a83094052eba9626fbf313791066d8a95a5929cf
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-06-05 08:56:53 -0600

    Remove requirements.txts

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import nox
 
 
-@nox.session(reuse_venv=True, venv_backend="venv")
+@nox.session(reuse_venv=True)
 def tests(session: nox.Session) -> None:
-    session.install("-r", "test-requirements.txt")
+    session.install("pytest", "sphinx")
     session.install(".")
     session.run("pytest")
